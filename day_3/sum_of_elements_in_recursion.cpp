2fd142adbfd9f9c397866b65ba7eed6557d4c804
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-11 19:54:35 +0530

    Create sum_of_elements_in_recursion.cpp

---
+++ day_3/sum_of_elements_in_recursion.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+int sub(int a[],int i,int n){
+    if(i>=n)
+    return 0;
+    else
+    return a[i]+sub(a,i+1,n);
+}
+int main() {
+    // Write C++ code here
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+   i=0;
+   cout<<sub(a,i,n);
+    return 0;
+}
+/*
+output
+5 
+1 2 8 4 9
+24
+  */
