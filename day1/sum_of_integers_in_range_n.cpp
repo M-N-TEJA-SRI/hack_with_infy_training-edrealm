1ecd35a98996d59b0db85925156d80356a3cd580
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-11 18:08:15 +0530

    Create sum_of_integers_in_range_n.cpp

---
+++ day1/sum_of_integers_in_range_n.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    cout<<(n*(n+1))/2;
+    return 0;
+}
+/*
+output
+5
+15
+  */
