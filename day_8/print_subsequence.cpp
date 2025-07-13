fe4ab1ce592685fca2c91c9a322bccca792a2bcb
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-13 18:35:11 +0530

    Create print_subsequence.cpp

---
+++ day_8/print_subsequence.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+int main() {
+   string s,t,lcs="";
+   cin>>s>>t;
+   int i,j,n=s.length(),m=t.length();
+   vector<vector<int>>v(n+1,vector<int>(m+1,0));
+for(i=1;i<=n;i++){
+    for(j=1;j<=m;j++){
+        if(s[i-1]==t[j-1]){
+        v[i][j]=1+v[i-1][j-1];
+            lcs+=s[i-1];
+        }
+        else
+        v[i][j]=max(v[i-1][j],v[i][j-1]);
+    }
+}
+   cout<<lcs;
+    return 0;
+}
+/*
+output
+abcd
+abd
+abd
+*/
